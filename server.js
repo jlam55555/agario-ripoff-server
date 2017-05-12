c9b1ab87a84edcd9f3b5a088a53f42fa1c445b2d
Jonathan Lam <jlam55555@gmail.com>
2017-05-12 14:45:13 -0400

    Added server disconnect code

--- server.js
+++ server.js
@@ -35,8 +35,7 @@ var checkSkittles = function() {
       var yDiff = player.y - skittle.y;
       var distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
       if(distance < 10*player.score) {
-	console.log(player.x, skittle.x, player.y, skittle.y, player.score);
-        player.score += 0.001;
+        player.score += 0.005;
         map.skittles.splice(map.skittles.indexOf(skittle), 1);
         break;
       }
@@ -47,14 +46,14 @@ setInterval(checkSkittles, 10);
 
 // listen to port
 http.listen(process.env.PORT || 5000, function() {
-  console.log('listening on *:' + (process.env.PORT || 5000));
+  console.log('Server listening on port ' + (process.env.PORT || 5000) + ".");
 });
 
 // socket stuff
 io.on("connection", function(socket) {
-  console.log("a new player has entered");
   var player;
   socket.on("name", function(playerName) {
+    console.log("Player \"" + name + "\" (id " + player.id + ") has entered. " + map.players.length + " players currently online.");
     player = new Player(playerName);
     map.players.push(player);
     socket.emit("player", player);
@@ -67,6 +66,10 @@ io.on("connection", function(socket) {
     });
   }); 
   socket.emit("mapDimensions", mapWidth, mapHeight);
+  socket.on("disconnect", function() {
+    map.players.splice(map.players.indexOf(player));
+    console.log("Player \"" + player.name + "\" (id " + player.id + ") has left. " + map.players.length + " players currently online.");
+  });
   
 });
 
